70a7558aaf4c5e56ad84f7ff970792369da09d42
Aanviiii <aanvi.24bcs10170@sst.scaler.com>
2025-04-11 16:16:03 +0530

    Creating the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,58 @@
 import React from "react";
+import "./Home.css";
+// import Upload from "./Upload";
+// import OutfitGenerator from "./OutfitGenerator";
+// import SavedCombos from "./SavedCombos";
+// import About from "./About";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      {/* <navLink to="/">
+        <Home />
+      </navLink>
+      <NavLink to="/upload">
+        <Upload />
+      </NavLink>
+      <NavLink to="/generate">
+        <OutfitGenerator />
+      </NavLink>
+      <NavLink to="/combos">
+        <SavedCombos />
+      </NavLink>
+      <NavLink to="/help">
+        <About />
+      </NavLink> */}
+
+      <nav>
+        <img href="" />
+        <h1>MyWardrobe</h1>
+        <h3>Your personal virtual closet✨</h3>
+        <div style={{ display: "flex", gap: "10px" }}>
+          <h5>Your Saved Combos</h5>
+          <h5>Your Wardrobe</h5>
+          <h5>About</h5>
+        </div>
+      </nav>
+      <div className="btns">
+        <button onClick={() => navigate("/upload")}>Get Started</button>
+        <button onClick={() => navigate("/generate")}>
+          Try Outfit Generator
+        </button>
+        <div>
+          <h2>How to use?</h2>
+          <div style={{ display: "flex", gap: "10px" }}>
+            <div className="cards">👚</div>
+            {/* Upload clothes */}
+            <div className="cards">🔀</div>
+            {/* Generate looks */}
+            <div className="cards">💾</div>
+            {/* Save outfits */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
