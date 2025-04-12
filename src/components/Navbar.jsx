df45b2439101b8bfa6da768c0fb2803c0bb706b6
Aanviiii <aanvi.24bcs10170@sst.scaler.com>
2025-04-12 12:35:29 +0530

    Creating the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import Logo from "../assets/MyWardrobe.png";
+import { Link } from "react-router-dom";
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <Link to="/">
+        <img src={Logo} />
+      </Link>
+
+      <Link to="/">Home</Link>
+
+      <Link to="/upload">Upload</Link>
+
+      <Link to="/wardrobe">Wardrobe</Link>
+
+      <Link to="/savedcombos">Saved Combos</Link>
+
+      <Link to="/outfitgenerator">Generater Outfits</Link>
+
+      <Link to="/about">About</Link>
+    </div>
+  );
 }
 
 export default Navbar;
